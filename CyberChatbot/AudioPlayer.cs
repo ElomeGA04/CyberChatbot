e5ec5be15a7b7ecd64f867e8159c63db5f265295
ElomeGA04 <ST10499189@rcconnect.edu.za>
2026-08-24 15:37:18 +0200

    Added ASCII Art

--- CyberChatbot/AudioPlayer.cs
+++ CyberChatbot/AudioPlayer.cs
@@ -1,10 +1,23 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using System.Media;
 
 namespace CyberChatbot
 {
-    internal class AudioPlayer
+    public class AudioPlayer
     {
+        public void PlayWelcomeMessage()
+        {
+            try
+            {
+                SoundPlayer player = new SoundPlayer("CyberWelcome.wav");
+                player.Play();
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Error!! The audio file could not be played. {ex.Message}");
+            }
+        }
     }
 }
---
+++ CyberChatbot/ResponseHandler.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CyberChatbot
+{
+    internal class ResponseHandler
+    {
+        public string Name { get; set; }
+    }
+}
