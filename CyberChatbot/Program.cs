2cce437a88b23d999c531c10b88f0eff6e6283dc
ElomeGA04 <ST10499189@rcconnect.edu.za>
2026-08-24 16:40:26 +0200

    Added a personalized greeting message for the user

--- CyberChatbot/Program.cs
+++ CyberChatbot/Program.cs
@@ -20,8 +20,18 @@ namespace CyberChatbot
 
             Console.WriteLine(@"====================================================================================================================================================================================================");
             Console.ResetColor();
+            Console.WriteLine();
 
-            Console.WriteLine("")
+            Console.WriteLine("==========================================================");
+            Console.WriteLine(" WELCOME TO THE CYBER CHATBOT! WE ARE HAPPY TO ASSIST YOU!");
+            Console.WriteLine("==========================================================");
+
+            ResponseHandler handle = new ResponseHandler();
+            
+            Console.WriteLine($"Please enter your name: {handle.Name}");
+            handle.Name = Console.ReadLine();
+            Console.WriteLine($"\nWelcome, {handle.Name}! You can start chatting with the Cyber Chatbot now.");
+
             Console.WriteLine("Press any key to exit...");
             Console.ReadKey();
         }
